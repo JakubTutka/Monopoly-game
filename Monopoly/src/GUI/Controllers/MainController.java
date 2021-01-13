b1413faea0a97b0a1390da7c6a3a21d6bf62e638
Kuba T <kuba.tutka@wp.pl>
2021-01-13 14:02:42 +0100

    basic GUI + menu, number of players window

---
+++ Monopoly/src/GUI/Controllers/MainController.java
@@ -0,0 +1,41 @@
+package GUI.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainController {
+
+    @FXML
+    private Button start;
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    void exitGame(ActionEvent event) {
+        Stage stageTheEventSourceNodeBelongs = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stageTheEventSourceNodeBelongs.close();
+    }
+
+    @FXML
+    void startGame(ActionEvent event) throws IOException {
+        Stage stageTheEventSourceNodeBelongs = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        FXMLLoader startGameLoader = new FXMLLoader(getClass().getResource("numberOfPlayers.fxml"));
+        Parent startGamePane = startGameLoader.load();
+        Scene scene = new Scene(startGamePane);
+
+        stageTheEventSourceNodeBelongs.setScene(scene);
+    }
+
+
+
+}
