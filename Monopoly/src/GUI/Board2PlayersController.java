c7aaed39cae78df4cbe38ac6196b832dcfbe7ead
Kuba T <kuba.tutka@wp.pl>
2021-01-19 13:06:08 +0100

    nvm

--- Monopoly/src/GUI/Board2PlayersController.java
+++ Monopoly/src/GUI/Board2PlayersController.java
@@ -3,6 +3,7 @@ package GUI;
 import Cells.*;
 import General.*;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
@@ -81,7 +82,7 @@ public class Board2PlayersController extends Thread{
 
     @FXML
     public void handluj(){
-
+        FXMLLoader startGameLoader = new FXMLLoader(getClass().getResource("FXML/.fxml"));
     }
 
     @FXML
